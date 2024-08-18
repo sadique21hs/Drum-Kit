5b5b1ef5f64d760ed840851cb2ce4be85803d39e
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-08-19 01:15:59 +0600

    Add sounds

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-var n=document.querySelectorAll(".drum").length;
+var n = document.querySelectorAll(".drum").length;
 
-for(var i=0; i<n; i++){
-     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    
+for (var i = 0; i < n; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonClick = this.innerHTML;
+    switch (buttonClick) {
+      case "w":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
 
-      var buttonClick= this.innerHTML;
-      switch (buttonClick) {
-        case "w":
-          var audio=new Audio("sounds/tom-1.mp3");
-      audio.play();
+      case "a":
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+
+      case "s":
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+
+        case "d":
+          var audio = new Audio("sounds/tom-4.mp3");
+          audio.play();
           break;
 
-          case "a":
-            var audio=new Audio("sounds/tom-2.mp3");
-      audio.play();
-      
-        default:
-          break;
-      }
-      
+          case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
 
-      
+            case "k":
+              var audio = new Audio("sounds/crash.mp3");
+              audio.play();
+              break;
+
+              case "l":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+
+      default:
+        break;
+    }
   });
-
 }
-
-
-
