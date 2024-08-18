789365078ec661c9a82eea39ddf4c739c5baca57
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-08-19 00:48:53 +0600

    Add sound

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var n=document.querySelectorAll(".drum").length;
 
 for(var i=0; i<n; i++){
      document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked");
+    
+      
+
+      var audio=new Audio("sounds/tom-1.mp3");
+      audio.play();
   });
 
 }
 
 
- 
+
