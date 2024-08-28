f169d753d1be59d6d6f577fbb2b63cfc7c90a66b
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-08-28 15:39:22 +0600

    Updated

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var n = document.querySelectorAll(".drum").length;
 for (var i = 0; i < n; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonClick = this.innerHTML;
+    makeSound(buttonInnerHTML);
+
+
     switch (buttonClick) {
       case "w":
         var audio = new Audio("sounds/tom-1.mp3");
@@ -46,9 +49,52 @@ for (var i = 0; i < n; i++) {
 
 
   document.addEventListener("keypress", function(event){
-   console.log(event);
+   makeSound(event.key)
   });
 
+
+
+  function makeSound(key){
+    switch (key) {
+      case "w":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
+
+      case "a":
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+
+      case "s":
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+
+      case "d":
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+        break;
+
+      case "j":
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+        break;
+
+      case "k":
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
+
+      case "l":
+        var audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+        break;
+
+      default:
+        break;
+    }
+  }
 
 
 }
