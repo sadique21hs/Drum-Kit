0ed12551c1bc21d3f9b9b10a29b3818232c984e9
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-08-28 15:28:25 +0600

    Updated

--- index.js
+++ index.js
@@ -19,28 +19,36 @@ for (var i = 0; i < n; i++) {
         audio.play();
         break;
 
-        case "d":
-          var audio = new Audio("sounds/tom-4.mp3");
-          audio.play();
-          break;
+      case "d":
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+        break;
 
-          case "j":
-            var audio = new Audio("sounds/snare.mp3");
-            audio.play();
-            break;
+      case "j":
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+        break;
 
-            case "k":
-              var audio = new Audio("sounds/crash.mp3");
-              audio.play();
-              break;
+      case "k":
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
 
-              case "l":
-                var audio = new Audio("sounds/kick-bass.mp3");
-                audio.play();
-                break;
+      case "l":
+        var audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+        break;
 
       default:
         break;
     }
   });
+
+
+  document.addEventListener("keypress", function(event){
+   console.log(event);
+  });
+
+
+
 }
